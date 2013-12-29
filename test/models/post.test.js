28f31de6255a5da140371ca41c3714daeb658bac
Tajima Junpei <p.baleine@gmail.com>
2013-12-29 17:48:30 +0900

    refactor

--- test/models/post.test.js
+++ test/models/post.test.js
@@ -1,5 +1,6 @@
+var co = require('co');
 var expect = require('chai').expect;
-var Promise = require('bluebird');
+var sinon = require('sinon');
 var _ = require('lodash');
 var User = require('../../lib/models/user');
 var Post = require('../../lib/models/post');
@@ -9,143 +10,126 @@ var knex = require('bookshelf').blogBookshelf.knex;
 
 describe('Post', function() {
 
-  function user(email, name, password) {
-    email = email || 'p.baleine@gmail.com';
-    name = name || 'p.baleine';
-    password = password || 'mysecret';
-
-    return new User({ email: email, name: name, password: password }).save();
-  }
-
-  beforeEach(function() {
-    return Promise.all([
-      knex('comments').del(),
-      knex('posts_tags').del(),
-      knex('posts').del(),
-      knex('users').del()
-    ]);
+  beforeEach(function(done) {
+    co(function *() {
+      yield knex('comments').del();
+      yield knex('posts_tags').del();
+      yield knex('posts').del();
+      yield knex('users').del();
+    })(done);
   });
 
   describe('relations', function() {
     it('should belong to User', function() {
-      return Promise.all([
-        expect(new Post().user().relatedData).to.exist,
-        expect(new Post().user().relatedData.target).to.equal(User),
-        expect(new Post().user().relatedData.type).to.equal('belongsTo')
-      ]);
+      expect(new Post().user().relatedData).to.exist;
+      expect(new Post().user().relatedData.target).to.equal(User);
+      expect(new Post().user().relatedData.type).to.equal('belongsTo');
     });
 
     it('should have many comments', function() {
-      return Promise.all([
-        expect(new Post().comments().relatedData).to.exist,
-        expect(new Post().comments().relatedData.target).to.equal(Comment),
-        expect(new Post().comments().relatedData.type).to.equal('hasMany')
-      ]);
+        expect(new Post().comments().relatedData).to.exist;
+        expect(new Post().comments().relatedData.target).to.equal(Comment);
+        expect(new Post().comments().relatedData.type).to.equal('hasMany');
     });
 
     it('should belong to many tags', function() {
-      return Promise.all([
-        expect(new Post().tags().relatedData).to.exist,
-        expect(new Post().tags().relatedData.target).to.equal(Tag),
-        expect(new Post().tags().relatedData.type).to.equal('belongsToMany')
-      ]);
+        expect(new Post().tags().relatedData).to.exist;
+        expect(new Post().tags().relatedData.target).to.equal(Tag);
+        expect(new Post().tags().relatedData.type).to.equal('belongsToMany');
     });
   });
 
   describe('#save()', function() {
     describe('tag attachement', function() {
-      it('should attach tags', function() {
-        var post = new Post({ title: 't1', content: 'c1', tags: [1, 2] });
-
-        return user().then(function(user) {
-          post.set('user_id', user.id);
-          return post.save();
-        }).then(function() {
-          return knex('posts_tags')
+      it('should attach tags', function(done) {
+        co(function *() {
+          var user = yield new User({ email: 'p.baleine@gmail.com', name: 'p', password: 's' }).save();
+          var post = yield new Post({ title: 't1', content: 'c1', user_id: user.id, tags: [1, 2] }).save();
+          var postsTags = yield knex('posts_tags')
             .where('post_id', post.id)
             .whereIn('tag_id', [1, 2])
             .orderBy('tag_id')
             .select();
-        }).then(function(postsTags) {
-          return Promise.all([
-            expect(postsTags[0].post_id).to.equal(post.id),
-            expect(postsTags[0].tag_id).to.equal(1),
-            expect(postsTags[1].post_id).to.equal(post.id),
-            expect(postsTags[1].tag_id).to.equal(2)
-          ]);
-        });
+
+            expect(postsTags[0].post_id).to.equal(post.id);
+            expect(postsTags[0].tag_id).to.equal(1);
+            expect(postsTags[1].post_id).to.equal(post.id);
+            expect(postsTags[1].tag_id).to.equal(2);          
+        })(done);
       });
 
-      it('should dettach tags', function() {
-        var post = new Post({ title: 't1', content: 'c1', tags: [1, 2] });
+      it('should dettach tags', function(done) {
+        co(function *() {
+          var user = yield new User({ email: 'p.baleine@gmail.com', name: 'p', password: 's' }).save();
+          var post = yield new Post({ title: 't1', content: 'c1', user_id: user.id, tags: [1, 2] }).save();
 
-        return user().then(function(user) {
-          post.set('user_id', user.id);
-          return post.save();
-        }).then(function() {
           post.set('tags', [2]); // detach tag 1
-          return post.save();
-        }).then(function() {
-          return knex('posts_tags')
+          yield post.save();
+
+          var postsTags = yield knex('posts_tags')
             .where('post_id', post.id)
             .whereIn('tag_id', [1, 2])
             .orderBy('tag_id')
             .select();
-        }).then(function(postsTags) {
-          return Promise.all([
-            expect(postsTags).to.have.length(1),
-            expect(postsTags[0].post_id).to.equal(post.id),
-            expect(postsTags[0].tag_id).to.equal(2)
-          ]);
-        });
+
+          expect(postsTags).to.have.length(1);
+          expect(postsTags[0].post_id).to.equal(post.id);
+          expect(postsTags[0].tag_id).to.equal(2);
+        })(done);
+      });
+
+      it('should not save when attaching tag failed', function(done) {
+        co(function *() {
+          var user = yield new User({ email: 'p.baleine@gmail.com', name: 'p', password: 's' }).save();
+          var post = new Post({ title: 't1', content: 'c1', tags: [1, 1], user_id: user.id });
+
+          // post.save() would failed due to duplicated id of tags.
+          expect((yield post.save())).to.equal(false);
+          expect((yield knex('posts').select())).to.be.empty;
+          
+        }).call(this, done);
       });
     });
 
     describe('validations', function() {
-      it('should not save post when the given title is empty', function() {
-        var post = new Post({ content: 'content1' });
+      it('should not save post when the given title is empty', function(done) {
+        co(function *() { 
+          var user = yield new User({ email: 'p.baleine@gmail.com', name: 'p', password: 's' }).save();
+          var post = new Post({ content: 'content1', user_id: user.id });
 
-        return user().then(function(user) {
-          post.set('user_id', user.id);
-          return post.save();
-        }).then(function(result) {
-          return Promise.all([
-            expect(result).to.equal(false),
-            expect(post.getValidationErrors()).to.contain('title is empty')
-          ]);
-        });
+          expect((yield post.save())).to.equal(false);
+          expect(post.getValidationErrors()).to.contain('title is empty');
+        })(done);
       });
 
-      it('should not save post when the given content is empty', function() {
-        var post = new Post({ title: 'title1' });
+      it('should not save post when the given content is empty', function(done) {
+        co(function *() {
+          var user = yield new User({ email: 'p.baleine@gmail.com', name: 'p', password: 's' }).save();
+          var post = new Post({ title: 'title1', user_id: user.id });
 
-        return user().then(function(user) {
-          post.set('user_id', user.id);
-          return post.save();
-        }).then(function(result) {
-          return Promise.all([
-            expect(result).to.equal(false),
-            expect(post.getValidationErrors()).to.contain('content is empty')
-          ]);
-        });
+          expect((yield post.save())).to.equal(false);
+          expect(post.getValidationErrors()).to.contain('content is empty');
+        })(done);
       });
     });
   });
 
   describe('.findAll()', function() {
-    beforeEach(function() {
-      return user().then(function(user) {
-          return Promise.all([
-            new Post({ title: 'title1', content: 'content1', user_id: user.id }).save(),
-            new Post({ title: 'title2', content: 'content2', user_id: user.id }).save()
-          ]);
-        });
+    beforeEach(function(done) {
+      co(function *() {
+        var user = yield new User({ email: 'p.baleine@gmail.com', name: 'p', password: 's' }).save();
+
+        yield new Post({ title: 'title1', content: 'content1', user_id: user.id }).save();
+        yield new Post({ title: 'title2', content: 'content2', user_id: user.id }).save();
+      })(done);
     });
 
-    it('should return all posts.', function() {
-      return Post.findAll().then(function(posts) {
-        return expect(posts).to.have.length(2);
-      });
-    });
+    it('should return all posts.', function(done) {
+      co(function *() {
+        var posts = yield Post.findAll();
+
+        expect(posts).to.have.length(2);
+      })(done);
+   });
   });
 });
