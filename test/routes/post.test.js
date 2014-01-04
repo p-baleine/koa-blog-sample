670570f2972a2e8f992f8f6001af57977d01e787
Tajima Junpei <p.baleine@gmail.com>
2014-01-04 12:57:50 +0900

    refactor post route test

--- test/routes/post.test.js
+++ test/routes/post.test.js
@@ -1,11 +1,14 @@
 var co = require('co');
+var _ = require('lodash');
 var path = require('path');
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var jade = require('jade');
-var Base = require('../../lib/models/base'); // establish connection
+var User = require('../../lib/models/user');
+var Post = require('../../lib/models/post');
 var Bookshelf = require('bookshelf').blogBookshelf;
 var post = require('../../lib/routes/post');
+var cleanupDb = require('../lib/cleanup-db')(Bookshelf.knex);
 
 var fixtures = [
   { title: 'title1', content: 'content1' },
@@ -24,7 +27,6 @@ describe('post route', function() {
 
   describe('GET /', function() {
     beforeEach(function() {
-      this.posts = Bookshelf.Collection.forge(fixtures, { model: Bookshelf.Model });
       this.fetchSpy = sinon.spy(Bookshelf.Collection.prototype, 'fetch');
       this.querySpy = sinon.spy(Bookshelf.Collection.prototype, 'query');
     });
@@ -63,41 +65,60 @@ describe('post route', function() {
   });
 
   describe('GET /:post', function() {
-    beforeEach(function() {
-      this.fetchStub = sinon.stub(Bookshelf.Model.prototype, 'fetch').returns({
-        then: function(cb) { cb(new Bookshelf.Model()); }
-      });
+    beforeEach(function(done) {
+      co(function *() {
+        this.user = yield new User({ email: 'a@a.com', name: 'aa', password: 'mysecret' }).save();
+        this.post = yield new Post({ title: 'title', content: 'content', user_id: this.user.id }).save();
+        this.fetchSpy = sinon.spy(Post.prototype, 'fetch');
+      }).call(this, done);
     });
 
     afterEach(function() {
-      Bookshelf.Model.prototype.fetch.restore();
+      Post.prototype.fetch.restore();
     });
 
     it('should fetch a post that has passed id', function(done) {
       co(function *() {
-        yield post.show(100);
+        yield post.show.call(this, this.post.id);
 
-        expect(this.fetchStub.lastCall.thisValue.id).to.equal(100);
+        expect(this.fetchSpy.lastCall.thisValue.id).to.equal(this.post.id);
       }).call(this, done);
     });
 
     it('should fetch a post with user, comments and tags', function(done) {
       co(function *() {
-        yield post.show;
+        yield post.show.call(this, this.post.id);
 
-        expect(this.fetchStub.lastCall.args[0]).to.have.been.property('withRelated');
-        expect(this.fetchStub.lastCall.args[0].withRelated).to.include('user');
-        expect(this.fetchStub.lastCall.args[0].withRelated).to.include('comments');
-        expect(this.fetchStub.lastCall.args[0].withRelated).to.include('tags');
+        expect(this.fetchSpy.lastCall.args[0]).to.have.been.property('withRelated');
+        expect(this.fetchSpy.lastCall.args[0].withRelated).to.include('user');
+        expect(this.fetchSpy.lastCall.args[0].withRelated).to.include('comments');
+        expect(this.fetchSpy.lastCall.args[0].withRelated).to.include('tags');
       }).call(this, done);
     });
 
     it('should render `post/show`', function(done) {
       co(function *() {
-        yield post.show;
+        yield post.show.call(this, this.post.id);
 
         expect(this.renderFileSpy.args[0][0]).to.match(/post\/show\.jade/);
       }).call(this, done);
+    });
+
+    describe('when the post dose not exist', function() {
+      it('should respond with error', function(done) {
+        var ctx = _.extend(this, {
+          throw: sinon.spy(function(message) {
+            throw new Error(message);
+          })
+        });
+
+        co(function *() {
+          yield post.show.call(this, this.post.id + 100);
+        }).call(ctx, function(e) {
+          expect(e.message).to.equal('404');
+          done();
+        });
+      });
     });
   });
 });
